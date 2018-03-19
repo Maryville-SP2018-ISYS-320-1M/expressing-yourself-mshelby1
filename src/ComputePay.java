1475571e06feb37c3f447234530af4fb3116f76a
mshelby1 <mshelby1@live.maryville.edu>
2018-03-19 01:12:41 -0500

    mshelby1
    Expressing Yourself

--- src/ComputePay.java
+++ src/ComputePay.java
@@ -6,13 +6,25 @@
 
 public class ComputePay {
 	public static void main(String[] args) {
-		System.out.println("My total hours worked:");
+		System.out.println( "My Total Hours Worked:");
+		Hours();
+		System.out.println();
+		System.out.println( "My Hourly Salary:");
+		Salary();
+		System.out.println();
+		System.out.println( "My Total Pay:");
+		Pay();
+	}
+	
+	public static void Hours() {
 		System.out.println(4 + 5 + 8 + 4);
-		
-		System.out.println("My hourly salary:");
-		System.out.println("$8.75");
-		
-		System.out.println("My total pay:");
-		System.out.println((4 + 5 + 8 + 4) * 8.75);
+	}
+	
+	public static void Salary() {
+		System.out.println(8.75);
+	}
+	
+	public static void Pay() {
+		System.out.println(21 * 8.75);
 	}
 }
