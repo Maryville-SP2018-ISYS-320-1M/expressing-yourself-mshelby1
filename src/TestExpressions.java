9c1fe32c160b75821405f1c3748ed03ab1825633
mshelby1 <mshelby1@live.maryville.edu>
2018-03-19 00:33:58 -0500

    mshelby1
    Expressing Yourself

--- src/TestExpressions.java
+++ src/TestExpressions.java
@@ -1,19 +1,19 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s): Matthew Shelby
+  	Date: 3/18/2018
 */
 
 /*
  	Your predicted values for the expressions
-	1.
-	2.
-	3.
-	4.
-	5.
-	6.
-	7.
-	8.
+	1. 8 correct
+	2. 6 correct
+	3. 3 correct
+	4. 4 correct
+	5. 2 correct
+	6. 11 correct
+	7. 2 + 234 correct
+	8. 72 + 2 correct
  
  	After testing the expressions in the main method, indicate above if your predictions were correct.
 */
@@ -21,7 +21,14 @@
 public class TestExpressions {
 
 	public static void main(String[] args) {
-		System.out.println("YOUR_TEST_EXPRESSION");
+		System.out.println(2 + 3 * 4 - 6);
+		System.out.println(14 / 7 * 2 + 30 / 5 + 1);
+		System.out.println((12 + 6) / 3 * 2);
+		System.out.println((238 % 10 + 3) % 7);
+		System.out.println(6 * 7 % 4);
+		System.out.println(2 + 2 + 3 + 4);
+		System.out.println("2 + 2" + 3 + 4);
+		System.out.println(3 + 4 + "2 + 2");
 	}
 
 }
